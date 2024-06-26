78d9f3bbf20ba09a1d7e9c823d5838e6e354a1dd
nhqb <nhqb@kali.nhqb-kali>
2024-06-25 21:38:57 -0400

    update database connection

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my-db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '10.0.10.51',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
